419111231b2ab5be765b6256514e52b1e178e8aa
Yemen <166258777+Sn4a5s6h@users.noreply.github.com>
2026-08-27 02:50:54 +0300

    Add chart of accounts with Arabic names
    Defines a set of accounting accounts with codes and names in Arabic.

---
+++ src/accounting-engine/chart-of-accounts.js
@@ -0,0 +1,44 @@
+const ACCOUNTS = {
+
+    CASH: {
+        code: "1100",
+        name: "الصندوق"
+    },
+
+    BANK: {
+        code: "1110",
+        name: "البنك"
+    },
+
+    ACCOUNTS_RECEIVABLE: {
+        code: "1200",
+        name: "العملاء"
+    },
+
+    INVENTORY: {
+        code: "1300",
+        name: "المخزون"
+    },
+
+    SALES: {
+        code: "4100",
+        name: "المبيعات"
+    },
+
+    SALES_RETURNS: {
+        code: "4200",
+        name: "مردودات المبيعات"
+    },
+
+    COST_OF_SALES: {
+        code: "5100",
+        name: "تكلفة المبيعات"
+    },
+
+    TAX_PAYABLE: {
+        code: "2100",
+        name: "ضريبة مستحقة"
+    }
+};
+
+module.exports = ACCOUNTS;
